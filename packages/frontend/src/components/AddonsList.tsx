ef54220708a23547ae60936f7e2b790fc84ebdc4
Viren070 <viren070@protonmail.com>
2025-01-02 19:49:07 +0000

    feat(frontend): sortable addon list

--- packages/frontend/src/components/AddonsList.tsx
+++ packages/frontend/src/components/AddonsList.tsx
@@ -40,6 +40,13 @@ const AddonsList: React.FC<AddonsListProps> = ({
     setAddons(newAddons);
   };
 
+  const moveAddon = (index: number, direction: 'up' | 'down') => {
+    const newAddons = [...addons];
+    const [movedAddon] = newAddons.splice(index, 1);
+    newAddons.splice(direction === 'up' ? index - 1 : index + 1, 0, movedAddon);
+    setAddons(newAddons);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.addonSelector}>
@@ -72,12 +79,79 @@ const AddonsList: React.FC<AddonsListProps> = ({
               <span style={{ fontSize: '1.2em', fontWeight: 'bold' }}>
                 {details?.name}
               </span>
-              <button
-                onClick={() => removeAddon(index)}
-                className={styles.deleteButton}
-              >
-                ✖
-              </button>
+              <div className={styles.actions}>
+                {index > 0 && (
+                  <button
+                    className={styles.actionButton}
+                    onClick={() => moveAddon(index, 'up')}
+                  >
+                    <svg
+                      fill="#ffffff"
+                      version="1.1"
+                      id="Capa_1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlnsXlink="http://www.w3.org/1999/xlink"
+                      width="24px"
+                      height="24px"
+                      viewBox="0 0 96.154 96.154"
+                      xmlSpace="preserve"
+                      stroke="#ffffff"
+                      transform="matrix(1, 0, 0, -1, 0, 0)"
+                    >
+                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></g>
+                      <g id="SVGRepo_iconCarrier">
+                        {' '}
+                        <g>
+                          {' '}
+                          <path d="M0.561,20.971l45.951,57.605c0.76,0.951,2.367,0.951,3.127,0l45.956-57.609c0.547-0.689,0.709-1.716,0.414-2.61 c-0.061-0.187-0.129-0.33-0.186-0.437c-0.351-0.65-1.025-1.056-1.765-1.056H2.093c-0.736,0-1.414,0.405-1.762,1.056 c-0.059,0.109-0.127,0.253-0.184,0.426C-0.15,19.251,0.011,20.28,0.561,20.971z"></path>{' '}
+                        </g>{' '}
+                      </g>
+                    </svg>
+                  </button>
+                )}
+                {index < addons.length - 1 && (
+                  <button
+                    className={styles.actionButton}
+                    onClick={() => moveAddon(index, 'down')}
+                  >
+                    <svg
+                      fill="#ffffff"
+                      version="1.1"
+                      id="Capa_1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24px"
+                      height="24px"
+                      viewBox="0 0 96.154 96.154"
+                      stroke="#ffffff"
+                    >
+                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></g>
+                      <g id="SVGRepo_iconCarrier">
+                        {' '}
+                        <g>
+                          {' '}
+                          <path d="M0.561,20.971l45.951,57.605c0.76,0.951,2.367,0.951,3.127,0l45.956-57.609c0.547-0.689,0.709-1.716,0.414-2.61 c-0.061-0.187-0.129-0.33-0.186-0.437c-0.351-0.65-1.025-1.056-1.765-1.056H2.093c-0.736,0-1.414,0.405-1.762,1.056 c-0.059,0.109-0.127,0.253-0.184,0.426C-0.15,19.251,0.011,20.28,0.561,20.971z"></path>{' '}
+                        </g>{' '}
+                      </g>
+                    </svg>
+                  </button>
+                )}
+                <button
+                  onClick={() => removeAddon(index)}
+                  className={styles.actionButton}
+                >
+                  ✖
+                </button>
+              </div>
             </div>
             <div className={styles.cardBody}>
               {details?.options?.map((option) => (
