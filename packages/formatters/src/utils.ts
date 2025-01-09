a36ff5f06647673b4230dee9e0310c1a9a403b10
Viren070 <viren070@protonmail.com>
2025-01-09 20:45:01 +0000

    feat(formatters): switch to base 2 units

--- packages/formatters/src/utils.ts
+++ packages/formatters/src/utils.ts
@@ -1,7 +1,7 @@
 export function formatSize(bytes: number): string {
   if (bytes === 0) return '0 B';
-  const k = 1000;
-  const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+  const k = 1024;
+  const sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB'];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
   return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
 }
