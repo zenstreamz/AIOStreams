156d0206b40d90034afa97502d7f90da9998009f
zenstreamz <thegospelofzen@tutanota.com>
2025-03-01 13:42:15 +0000

    Update base.ts

--- packages/wrappers/src/base.ts
+++ packages/wrappers/src/base.ts
@@ -14,7 +14,6 @@ import {
   Settings,
   createLogger,
 } from '@aiostreams/utils';
-import { fetch as uFetch, ProxyAgent } from 'undici';
 import { emojiToLanguage, codeToLanguage } from '@aiostreams/formatters';
 
 const logger = createLogger('wrappers');
@@ -157,8 +156,7 @@ export class BaseWrapper {
     );
 
     let response = useProxy
-      ? uFetch(url, {
-          dispatcher: new ProxyAgent(Settings.ADDON_PROXY),
+      ? fetch(url, {
           method: 'GET',
           headers: headers,
           signal: AbortSignal.timeout(this.indexerTimeout),
